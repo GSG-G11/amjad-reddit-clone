59776e201d804b42a46308277f9238dc48a7efb2
Amjad <amjadyehia1998@gmail.com>
2022-03-12 05:25:33 +0200

    move dotenv require to server/index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
+require('dotenv').config();
+
 const port = process.env.PORT;
-require('./app').listen(
-  process.env.PORT,
-  () => console.log(`app is live on http://localhost:${port}`),
-);
+
+require('./app').listen(process.env.PORT, () => console.log(`app is live on http://localhost:${port}`));
