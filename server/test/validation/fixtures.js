538dba948968b6e2d20316aa2a2cf6e160d0ab7f
Amjad <amjadyehia1998@gmail.com>
2022-03-12 09:58:23 +0200

    add test for post signup and add fixture file relates #7

---
+++ server/test/validation/fixtures.js
@@ -0,0 +1,8 @@
+const dummyUser = {
+  name: 'Amjad',
+  email: 'amjad@gmail.com',
+  password: '123456',
+  confirmPassword: '123456',
+};
+
+module.exports = { dummyUser };
--- server/test/validation/signup.test.js
+++ server/test/validation/signup.test.js
@@ -1,7 +1,10 @@
 const req = require('supertest');
 const app = require('../../app');
 
+const dummyUser = require('./fixtures');
+
 describe('GET/signup', () => {
+  // GET /signup
   it('should respond with 200 and text/html when successful', (done) => {
     req(app)
       .get('/api/v1/signup')
@@ -11,6 +14,23 @@ describe('GET/signup', () => {
         if (err) return done(err);
 
         expect(res.ok).toBeTruthy();
+        return done();
+      });
+  });
+
+  // POST /signup
+  it('should respond with 201 and text/plain when successful', (done) => {
+    req(app)
+      .post('/api/v1/signup')
+      .expect(201)
+      .send(dummyUser)
+      .expect('content-type', 'application/json; charset=utf-8')
+      .end((err, { body }) => {
+        if (err) return done(err);
+
+        const expectedMsg = 'account created successfully';
+
+        expect(body.msg).toEqual(expectedMsg);
         return done();
       });
   });
