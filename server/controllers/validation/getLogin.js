32ffbb79e4f1a496210d1c619e463e896c6f608b
Amjad <amjadyehia1998@gmail.com>
2022-03-13 19:06:01 +0200

    add auth check to GET/login route relates #11

--- server/controllers/validation/getLogin.js
+++ server/controllers/validation/getLogin.js
@@ -1,6 +1,8 @@
 const { join } = require('path');
 
-const getLogin = (req, res) => {
+const getLogin = ({ id }, res) => {
+  if (id) return res.status(303).send({ msg: 'you are already authenticated, redirecting...' });
+
   res.status(200).sendFile(join(__dirname, '..', '..', '..', 'public', 'pages', 'login.html'));
 };
 
--- server/routers/validation.js
+++ server/routers/validation.js
@@ -1,12 +1,10 @@
 const validRouter = require('express').Router();
 const {
-  handleGetSignUp,
-  handlePostSignUp,
-  handleGetLogin,
-  handlePostLogin,
+  handleGetSignUp, handlePostSignUp, handleGetLogin, handlePostLogin,
 } = require('../controllers');
+const auth = require('../middlewares/auth');
 
 validRouter.route('/signup').get(handleGetSignUp).post(handlePostSignUp);
-validRouter.route('/login').get(handleGetLogin).post(handlePostLogin);
+validRouter.route('/login').get(auth, handleGetLogin).post(handlePostLogin);
 
 module.exports = validRouter;
