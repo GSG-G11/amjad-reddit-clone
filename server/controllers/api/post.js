9bf4438fd1a14d9dcc5a6481eb2efbe6c0efbd5b
Amjad <amjadyehia1998@gmail.com>
2022-03-15 10:49:23 +0200

    add tests for post route relates #14

--- server/controllers/api/post.js
+++ server/controllers/api/post.js
@@ -7,11 +7,13 @@ const {
 } = require('../../database/queries/api/post');
 const { destructure, checkAuth } = require('../../utils');
 
+// * get all posts
 const getAllPosts = (req, res, next) =>
   getAllPostsQuery()
     .then(({ rows }) => res.json(rows))
     .catch(next);
 
+// * get user's posts
 const getPost = (req, res, next) => {
   const { tokenId, userId } = destructure(req);
 
@@ -22,33 +24,35 @@ const getPost = (req, res, next) => {
     .catch(next);
 };
 
+// * create post
 const createPost = (req, res, next) => {
   const { tokenId, userId, content, date } = destructure(req);
 
   checkAuth(tokenId, userId);
 
   return createPostQuery({ content, date, userId })
-    .then(() => res.end())
+    .then(() => res.status(201).end())
     .catch(next);
 };
 
+// * edit post
 const editPost = (req, res, next) => {
   const { tokenId, userId, postId, content } = destructure(req);
 
   checkAuth(tokenId, userId);
-
   return editPostQuery({ content, postId, userId })
-    .then(() => res.end())
+    .then(() => res.status(204).end())
     .catch(next);
 };
 
+// * delete post
 const deletePost = (req, res, next) => {
   const { tokenId, userId, postId } = destructure(req);
 
   checkAuth(tokenId, userId);
 
   deletePostQuery({ userId, postId })
-    .then(() => res.end())
+    .then(() => res.status(204).end())
     .catch(next);
 };
 
